02a16e82f65277fb975bc4f8220e4bf7efb843ab
Costa Nonato <costa@nonato.com>
2022-03-05 15:57:33 -0300

    iniciando teste users_controller_rspec

---
+++ spec/requests/api/v1/users_request_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Users", type: :request do
+  let!(:user) {create(:user) }
+  let(:user_id){ user.id }
+
+  describe "GET /users/:id" do
+    before do
+      headers = { 'Accept' => "aplication/vnd.taskmanager.v1" }
+      get "/users/#{user_id}", {}, headers
+    end
+
+    context "when the user exists" do
+      if "returns the user" do
+        user_response = JSON.parse(response.body)
+        expect(user_response["id"]).to eq(user_id)
+      end
+    end
+
+  end
+
+end
