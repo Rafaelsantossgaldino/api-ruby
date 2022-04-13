6e6cd0a874c2ff0146c7bd3aa4e843c46caeb257
Costa Nonato <costa@nonato.com>
2022-04-13 14:30:28 -0300

    reajuste e implementando teste action create

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Api::V1::Users", type: :request do
 
   describe "GET /users/:id" do
     before do
-      headers = { 'Accept' => "aplication/vnd.taskmanager.v1" }
+      headers = { 'Accept' => "application/vnd.taskmanager.v1" }
       get "/users/#{user_id}", params: {}, headers: headers
     end
 
@@ -24,11 +24,41 @@ RSpec.describe "Api::V1::Users", type: :request do
     end
 
     context "when the user does not exist" do
-      let!(:user_id) { 1000 }
+      let!(:user_id) { 10000 }
 
       it "returns status code 404" do
         expect(response).to have_http_status(404)
       end
     end
   end
+
+  describe "POST /users" do
+    before do
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1'}
+      post "/users", params: { user: user_params }, headers: headers
+    end
+    context " when the request params are valid" do
+      let(:user_params) { attributes_for(:user) }
+
+      it "returns status code 201" do
+        expect(response).to have_http_status(201)
+      end
+      it "returns json data for the created user" do
+        user_response = JSON.parse(response.body)
+        expect(user_response['email']).to eq(user_params[:email])
+      end
+    end
+    context " when the request params are invalid" do
+      let(:user_params) { attributes_for(:user, email: 'invalid email@') }
+
+      it "return status code 422" do
+        expect(response).to have_http_status(422)
+      end
+      it "returns the json data for the erros" do
+        user_response = JSON.parse(response.body)
+        expect(user_response).to have_key('errors')
+      end
+    end
+  end
+  
 end
