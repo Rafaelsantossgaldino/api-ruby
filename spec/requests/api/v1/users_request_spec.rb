c25a1c9eaba06dba637ec0a630c2e58757af3880
Costa Nonato <costa@nonato.com>
2022-04-13 16:01:46 -0300

    teste rspec action update

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -60,5 +60,32 @@ RSpec.describe "Api::V1::Users", type: :request do
       end
     end
   end
+
+  describe "PUT /users/:id" do
+    before do
+      put "/users/#{user_id}", params: { user: user_params }, headers: headers
+    end
+    context " when the request params are valid" do
+      let(:user_params) { { email: 'new_email@taskmanager.com' } }
+      it "returns status code 200" do
+        expect(response).to have_http_status(200)
+      end
+      it "returns the json data for the update user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(user_params[:email])
+      end
+    end
+    context " when the request params are invalid" do
+      let(:user_params) { attributes_for(:user, email: 'invalid email@') }
+
+      it "return status code 422" do
+        expect(response).to have_http_status(422)
+      end
+      it "returns the json data for the erros" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+    end
+  end
   
 end
