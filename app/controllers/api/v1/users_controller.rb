47a00fb5ce0cdb9fdd1b944e3ec6fcfc082c76f6
Costa Nonato <costa@nonato.com>
2022-04-13 11:11:04 -0300

    rspec test endpoint api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::UsersController < ApplicationController
-  
-  def index
-      
+  respond_to :json
+
+  def show
+    begin
+      @user = User.find(params[:id])
+      respond_with @user
+    rescue 
+      head 404
+    end 
   end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -4,19 +4,31 @@ RSpec.describe "Api::V1::Users", type: :request do
   let!(:user) {create(:user) }
   let(:user_id){ user.id }
 
+  before { host! "api.taskmanager.dev"}
+
   describe "GET /users/:id" do
     before do
       headers = { 'Accept' => "aplication/vnd.taskmanager.v1" }
-      get "/users/#{user_id}", {}, headers
+      get "/users/#{user_id}", params: {}, headers: headers
     end
 
     context "when the user exists" do
-      if "returns the user" do
+      it "returns the user" do
         user_response = JSON.parse(response.body)
         expect(user_response["id"]).to eq(user_id)
       end
+
+      it "returns status code 200"do
+        expect(response).to have_http_status(200)
+      end
     end
 
-  end
+    context "when the user does not exist" do
+      let!(:user_id) { 1000 }
 
+      it "returns status code 404" do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 end
